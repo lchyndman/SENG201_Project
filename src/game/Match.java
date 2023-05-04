fc02a2038ed962c644012c48c2698993c3b0aab3
Kva53 <kva53@uclive.ac.nz>
2023-05-04 12:30:18 +1200

    changed match

--- src/game/Match.java
+++ src/game/Match.java
@@ -5,115 +5,135 @@ import java.util.ArrayList;
 
 public class Match {
 	
-	private int maxOvers;
+	private int maxOvers = 20;
 	private int currentOver = 0;
 	
-//	private boolean allInjurd;
-	
-	private Team team;
-//	private ArrayList battingOrder;
-//	private Array bowlerOrder;        these are part of team? team.bowlerOrder();
+	private Team playerTeam;
 	private Athlete currentBatter;
 	private Athlete currentBowler;
-	private int currentScore = 0;
-	
 	
 	private Team opponentTeam;
-//	private ArrayList opponentBowlerOrder;
-//	private ArrayList opponentBattingOrder;
 	private Athlete opponentCurrentBatter;
 	private Athlete opponentCurrentBowler;
 	private int opponentCurrentScore;
 	
+	
 	public String playMatch() {
+		int playerFinalScore = playHalf(playerTeam, opponentTeam);
+		int opponentFinalScore = playHalf(opponentTeam,playerTeam);
+		if (playerFinalScore > opponentFinalScore) {
+			return "Player team wins";
+		}
+		if (playerFinalScore < opponentFinalScore) {
+			return "Opponent wins";
+		}
+		else {
+			return "It was a draw";
+		}
 		
-//		faces off each athlete on players team with the corresponding athlete on 
-//		opponents team. then visa versa (batter vs bowler)
-//		uses faceOff method, if injured method
-//		goes while numovers <= maxovers and not all players are injured
+	}
+	
+	public int playHalf(Team team1, Team team2) {
+		
+//		faces off each athlete on players playTeam with the corresponding athlete on 
+//		opponents playTeam. then visa versa (batter vs bowler)
+//		uses faceOff method
+//		goes while numovers <= maxovers 
 //		small chance of being caught by fielder
 //		collects total scores
-//		returns the name of winning team
+//		returns the name of winning playTeam
 		
 		
-//		we bat first 
-//		make the match run twice, change this
-		Team battingTeam = team;
-		Team bowlingTeam = opponentTeam;
-//		will this change the original team?
+//		player bats first 
+		Team battingTeam = team1;
+		Team bowlingTeam = team2;
 		
-
-		int lenBat = battingTeam.battingOrder.size();
+		int lenBat = battingTeam.getBattingOrder.size();
 		int batIn = 0;
-		currentBatter = battingTeam.battingOrder.get(batIn);
+		Athlete currentBatter = battingTeam.getBattingOrder.get(batIn);
 		int battersthree = 0;
 		
-		int lenBowl = bowlingTeam.bowlingOrder.size();
+		int lenBowl = bowlingTeam.getBowlingOrder().length;
 		int bowlIn = 0;
-		currentBowler = bowlingTeam.bowlingOrder.get(bowlIn);
-
-		int lenBat = battingTeam.getBattingOrder().size();
-		int batIn = 0;
-		currentBatter = battingTeam.getBattingOrder().get(batIn);
-		int battersthree = 0;
-		
-		int lenBowl = bowlingTeam.getBowlingOrder().size();
-		int bowlIn = 0;
-		currentBowler = bowlingTeam.getBowlingOrder().get(bowlIn);
+		Athlete currentBowler = bowlingTeam.getBowlingOrder()[bowlIn];
 
 		int bowlerThree = 0;
 	
-		
+		int currentScore = 0;
 
-		
-	
 		for (currentOver=0; currentOver < maxOvers; currentOver += 1) {
 			
-			boolean batterOut = false;
+			while (batIn < lenBat) {    // stops count if all batters out before end of overs
 			
-			int points = faceOff(currentBatter, currentBowler);
-			
-			if (points < 0) {
-//				batter out on wicket
-				batterOut = true;
-			}
-			if (0 <= points && points <= 4) {
-//				chance it could get caught, currently set for 5% (later base on fielding stat)
-				int number = getRandomNumber(0,100);
-				if (number <= 5) {
-//					batsman is caught out, next batter
-					batterOut = true;
-					}
-			}
+				boolean batterOut = false;
 				
-			if (batterOut) {
-				batIn += 1;
-				currentBatter = battingTeam.batterOrder.get(batIn);
+				
+				
+				int points = faceOff(currentBatter, currentBowler);
+				
+				boolean batterfreakinjury = false;
+				int risk = currentBatter.getStamina();
+				int random = getRandomNumber(0,1000);
+				if (random < risk) {
+					batterfreakinjury = true;
+//					set player injured
 				}
-			else {
-				currentScore += points;
-				battersthree += 1;
-				if (battersthree % 3 ==0) {
-					battersthree = 0;
-					batIn += 1;
-					currentBatter = battingTeam.batterOrder.get(batIn);
 				
-			}
-			
-			
-//			what do i do if batters run out before overs? does this ever happen? #askluke
-			}
-			
-			
-			bowlerThree += 1;
-			if (bowlerThree % 3 ==0) {
-				bowlIn += 1;
-				currentBowler = bowlingTeam.bowlerOrder(bowlIn);
+				boolean bowlerfreakinjury = false;
+				int risk1 = currentBowler.getStamina();
+				int random1 = getRandomNumber(0,1000);
+				if (random1 < risk1) {
+					bowlerfreakinjury = true;
+//				set player injured
+				}
+				
+				
+				int fieldStat = 0;
+				Athlete[] fieldingPlayers = bowlingTeam.getBowlingOrder();
+				for (Athlete fielder : fieldingPlayers) {
+					fieldStat += fielder.getFielding();
+				}
+				fieldStat = fieldStat / 100;
+				
+				if (points < 0) {
+	//				batter out on wicket
+					batterOut = true;
+				}
+				if (0 <= points && points <= 4) {
+	//				chance it could get caught, based on fielding stat)
+					int number = getRandomNumber(0,100);
+					if (number <= fieldStat) {
+						batterOut = true;
+						}
+				}
+				
+					
+				if (batterOut || batterfreakinjury ) {
+					batIn += 1;
+					currentBatter.batOver(5);
+					currentBatter = battingTeam.batterOrder.get(batIn);
+					battersthree = 0;
+					}
+				else {
+					currentScore += points;
+					}
+				
+				
+				if (bowlerfreakinjury)  {
+//					bowlIn += 1;
+					currentBowler = bowlingTeam.bowlerOrder(bowlIn);
+					bowlerThree = 0;
+				}
+				bowlerThree += 1;
+				if (bowlerThree % 3 ==0) {
+					bowlIn += 1;
+					bowlerThree = 0;
+					currentBowler = bowlingTeam.bowlerOrder(bowlIn);
+				}
 			}
 		}
 		
-//		return score current score
-//		need a way to switch team and differentiate the total scores of team and opponents
+		return currentScore;
 	}
 				
 			
@@ -123,6 +143,10 @@ public class Match {
 	public int faceOff(Athlete batter, Athlete bowler) {
 //		returns the score of the batter using our awesome face-off idea
 //		uses random num generator
+		bowler.bowlOver();
+		
+		
+		
 		int base_result = 500;
 		int batting = batter.getBatting;
 		int bowling = bowler.getBowling;
@@ -142,22 +166,26 @@ public class Match {
 			    (755 <= num && num <= 890) ? 4 : 6 ) {
 		
 		
-		case -1:
-
-			return -1;
-			break;
-			
+		case -1:     //			ball hit wicket, out
+			batter.batOver(2);
+			return -1;		
 		case 0:
+			batter.batOver(2);
 			return 0;
 		case 1:
+			batter.batOver(1);
 			return 1;
 		case 2:
+			batter.batOver(2);
 			return 2;
 		case 3:
+			batter.batOver(3);
 			return 3;
 		case 4:
+			batter.batOver(1);
 			return 4;
 		case 6:
+			batter.batOver(0);
 			return 6;
 			
 			
